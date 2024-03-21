c1f41f15a342623a2f79db1da7d7b1f585e9ad0f
R Pritto Ruban <prittoruban0609@gmail.com>
2024-03-21 20:18:50 +0530

    Update 4th.cpp

--- Part Eleven Algorithms/4th.cpp
+++ Part Eleven Algorithms/4th.cpp
@@ -20,48 +20,4 @@ Max Execution Time Limit: 50 millisecs
 */
 
 
-#include<bits/stdc++.h>
-using namespace std;
-
-void printArray(vector<int>& arr) {
-    for(int i = 0; i < arr.size(); i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
-
-int partition(vector<int>& arr, int low, int high) {
-    int pivot = arr[high];  
-    int i = (low - 1);  
-
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] <= pivot) {  // Changed from arr[j] < pivot
-            i++;  
-            swap(arr[i], arr[j]);
-        }
-    }
-    swap(arr[i + 1], arr[high]);
-    return (i + 1);
-}
-
-void quickSort(vector<int>& arr, int low, int high) {
-    if(low < high) {
-        int pi = partition(arr, low, high);
-        if (low == 0 && high == arr.size() - 1) {
-            printArray(arr);  // print array after each partition
-        }
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
-
-int main() {
-    int N;
-    cin >> N;
-    vector<int> arr(N);
-    for(int i = 0; i < N; i++) {
-        cin >> arr[i];
-    }
-    quickSort(arr, 0, N-1);
-    printArray(arr);  // print array after it is fully sorted
-}
+code will be uploaded soon
