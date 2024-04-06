00002691f2479cb5fc5a4357d6a8cef14a970f5f
R Pritto Ruban <prittoruban0609@gmail.com>
2024-04-06 20:06:43 +0530

    Update 4th.cpp

--- Part Eleven Algorithms/4th.cpp
+++ Part Eleven Algorithms/4th.cpp
@@ -20,4 +20,50 @@ Max Execution Time Limit: 50 millisecs
 */
 
 
-code will be uploaded soon
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArray(const vector<int>& arr) {
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+}
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[low];
+    int i = low - 1;
+    int j = high + 1;
+
+    while (true) {
+        do {
+            i++;
+        } while (arr[i] < pivot);
+        do {
+            j--;
+        } while (arr[j] > pivot);
+
+        if (i >= j) return j;
+        swap(arr[i], arr[j]);
+        printArray(arr);
+    }
+}
+
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pivot_index = partition(arr, low, high);
+        quickSort(arr, low, pivot_index);
+        quickSort(arr, pivot_index + 1, high);
+    }
+}
+
+int main(int argc, char** argv) {
+    int N;
+    cin >> N;
+    vector<int> arr(N);
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    quickSort(arr, 0, N - 1);
+    return 0;
+}
